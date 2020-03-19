6c2a7c6b4ea387c4449fad4baf199341c6142829
gubarbosa <gugabarbosa.bsi@gmail.com>
2020-03-19 03:26:10 -0300

    fazendo json

--- index.py
+++ index.py
@@ -3,11 +3,13 @@ import json
 import string
 import hashlib
 
-requisicao = https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=dbe5421d5d8160f6094d1f402b28c8be16f28f95
-r = requests.get(requesicao)
+#faz a requisicao HTTP
+requisicao = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=dbe5421d5d8160f6094d1f402b28c8be16f28f95"
+r = requests.get(requisicao)
 
 #conteudo json
 json = json.dumps(r.json())
 
+#cria um arquivo json com o conteudo da requisicao
 arquivo = open("answer.json", "w")
 arquivo.write(json)
